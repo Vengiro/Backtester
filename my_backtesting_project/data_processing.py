c17ef34b6d4bb38c389e95754e20777a6e0f7bf3
Vengiro <120104662+Vengiro@users.noreply.github.com>
2024-10-24 01:54:40 -0400

    Basic class and main

--- my_backtesting_project/data_processing.py
+++ my_backtesting_project/data_processing.py
@@ -1,5 +1,32 @@
 import yfinance as yf
+import matplotlib.pyplot as plt
 
-def get_historical_data(ticker, start_date, end_date):
-    data = yf.download(ticker, start=start_date, end=end_date)
-    return data
+class Backtester:
+    def __init__(self, ticker: str, balance: float):
+        self.ticker = ticker
+        self.data = yf.Ticker(ticker).history(period="1d")
+        self.data["Return"] = self.data["Close"].pct_change()
+        self.data["Cumulative Return"] = (1 + self.data["Return"]).cumprod()
+        self.data["Cumulative Return"].plot()
+        self.balance = balance
+        self.shares = 0
+        self.purchases = []
+        self.sells = []
+        plt.show()
+
+
+    def get_historical_data(self, start_date, end_date):
+        data = yf.download(ticker, start=start_date, end=end_date)
+        return data
+
+    def buy(self, date, amount):
+        price = self.data.loc[date, "Close"]
+        self.shares += amount / price
+        self.balance -= amount
+        self.purchases.append(date)
+
+    def sell(self, date, amount):
+        price = self.data.loc[date, "Close"]
+        self.shares -= amount / price
+        self.balance += amount
+        self.sells.append(date)
